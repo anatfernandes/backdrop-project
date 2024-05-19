e7f5514c3b21268144d3d37532d8ca5bdc8564d6
anatfernandes <analuizateixeirafernandes184@gmail.com>
2024-05-18 23:36:13 -0300

    🐛 | fix(back): increase the limit of listed topics

--- backend/src/repositories/topic/topic.repository.ts
+++ backend/src/repositories/topic/topic.repository.ts
@@ -3,7 +3,7 @@ import { handleQuery } from "../../database";
 import { ListTopicsResultType } from "./types";
 
 async function listTopics() {
-  const query = fql`Topics.byNameAsc();`;
+  const query = fql`Topics.byNameAsc().pageSize(100);`;
   const result = (await handleQuery(query)) as ListTopicsResultType;
   return result?.data;
 }
