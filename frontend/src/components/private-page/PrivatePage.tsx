1f18a67390f3985524ada5cef710f40b46a446a8
anatfernandes <analuizateixeirafernandes184@gmail.com>
2024-05-27 13:17:28 -0300

    ✨ | feat(front): redirect user to finish profile on sign in

--- frontend/src/components/private-page/PrivatePage.tsx
+++ frontend/src/components/private-page/PrivatePage.tsx
@@ -1,20 +1,27 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { useLocale, useToast, useUser } from "../../hooks";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useLocalStorage, useLocale, useToast } from "../../hooks";
 
 function PrivatePage({ children }: Readonly<React.PropsWithChildren>) {
-  const { user } = useUser();
+  const { localStorageData: user } = useLocalStorage();
   const { t } = useLocale();
   const toast = useToast();
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     if (!user.token) {
       navigate("/sign-in");
       toast({ text: t("Common.NotLoggedUserError"), type: "error" });
+      return;
     }
-  }, [user]);
+
+    if (!user?.topics?.length) {
+      navigate("/settings/profile");
+      toast({ text: t("User.FinishProfile"), type: "warning" });
+    }
+  }, [user.id, location.pathname]);
 
   return <>{children}</>;
 }
